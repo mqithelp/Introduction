8ca78a157f0a4fb903b1ba7d1b4f0c3c320b3b57
Maxim <mqithelp@yandex.ru>
2024-06-05 19:41:56 +0300

    Task 1 is done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("\n==========[ Task 1 ]==========\n")
-        ;
+        System.out.printf("\n==========[ Task 1 ]==========\n");
+        short clientOS;
+
+        String identifyOS = "Android";
+
+        if (identifyOS.equals("iOS")) {
+            clientOS = 0;
+            System.out.printf("Установите версию приложения для iOS по ссылке.\n");
+        } else if (identifyOS.equals("Android")) {
+            clientOS = 1;
+            System.out.printf("Установите версию приложения для Android по ссылке.\n");
+        }
     }
 }
