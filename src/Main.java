f90075fa36470fba4c224826fbe220366091d214
Maxim <mqithelp@yandex.ru>
2024-06-05 19:51:44 +0300

    Task 2 is done

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.printf("\n==========[ Task 1 ]==========\n");
-        short clientOS;
+        short clientOS=0;
 
-        String identifyOS = "Android";
+        String identifyOS = "iOS";
 
         if (identifyOS.equals("iOS")) {
             clientOS = 0;
@@ -11,6 +11,23 @@ public class Main {
         } else if (identifyOS.equals("Android")) {
             clientOS = 1;
             System.out.printf("Установите версию приложения для Android по ссылке.\n");
+        }
+
+        System.out.printf("\n==========[ Task 2 ]==========\n");
+        int clientDeviceYear = 2015;
+
+        if (clientDeviceYear < 2015) {
+            if (clientOS == 0) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        } else {
+            if (clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
         }
     }
 }
