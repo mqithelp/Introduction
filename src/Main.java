873db966508ef2b90b0afc9152f8680a37386838
mqithelp <79203576+mqithelp@users.noreply.github.com>
2024-06-06 15:34:47 +0300

    Hw 5.conditional operator (#2)
    * Init branch commit
    
    * Task 1 is done
    
    * Task 2 is done
    
    * Task 3 is done
    
    * Task 4. Рабочая версия 1.
    
    * Task 4 is done.
    
    * Task 5 is done.
    
    * Исправил неточности.
    
    * Задача 4. Подправил диапазоны.
    
    ---------
    
    Co-authored-by: Maxim <mqithelp@yandex.ru>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
 public class Main {
     public static void main(String[] args) {
+        Task1_2();
+        Task3();
+        Task4();
+        Task5();
+    }
+
+    private static void Task1_2() {
+        System.out.printf("\n==========[ Task 1 ]==========\n");
+        short clientOS = 0;
+
+        if (clientOS == 0) {
+            System.out.printf("Установите версию приложения для iOS по ссылке.\n");
+        } else  {
+            System.out.printf("Установите версию приложения для Android по ссылке.\n");
+        }
+
+        System.out.printf("\n==========[ Task 2 ]==========\n");
+        int clientDeviceYear = 2015;
+
+        if (clientDeviceYear < 2015) {
+            if (clientOS == 0) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        } else {
+            if (clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+    }
+
+    private static void Task3() {
+        System.out.printf("\n==========[ Task 3 ]==========\n");
+        int year = 2011;
+        if (year > 1584 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
+            System.out.printf("%d год является високосным.\n", year);
+        } else if (year > 1584) {
+            System.out.printf("%d год не является високосным.\n", year);
+        } else {
+            System.out.println("Введен некорректный год.");
+        }
 
         Task1();
         Task2();
@@ -169,5 +213,53 @@ public class Main {
             max = three;
         }
         System.out.printf("Максимальное число из %d, %d, %d равно %d\n", one, two, three, max);
+    }
+
+    private static void Task4() {
+        System.out.printf("\n==========[ Task 4 ]==========\n");
+        int deliveryDistance = 100;
+        int dayDelivery = 0;
+        if (deliveryDistance <= 20) {
+            dayDelivery += 1;
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            dayDelivery += 2;
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            dayDelivery += 3;
+        } else {
+            System.out.println("Доставки нет.");
+        }
+        if (dayDelivery > 0) {
+            System.out.printf("Потребуется дней: %d\n",dayDelivery);
+        }
+    }
+
+    private static void Task5() {
+        System.out.printf("\n==========[ Task 5 ]==========\n");
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима.");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна.");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето.");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень.");
+                break;
+            default:
+                System.out.println("Месяц указан неверно.");
+                break;
+        }
     }
 }
