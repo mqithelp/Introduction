20528bfa6f692bb15a66f6e4e2855bab13a1c660
MaxHome <mqithelp@gmail.com>
2024-06-10 21:23:17 +0300

    Task 2 is done.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
             Task1();
+            Task2();
            }
+
+    private static void Task2() {
+        System.out.println("\n==========[ Task 2 ]==========");
+        for (int i = 10; i >= 1 ; i--) {
+            System.out.printf("%d\t",i);
+        }
+    }
 
     private static void Task1() {
         System.out.println("\n==========[ Task 1 ]==========");
