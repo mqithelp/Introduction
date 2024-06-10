763a2afc15796be10103c8bbdfd48f3437b9c821
MaxHome <mqithelp@gmail.com>
2024-06-10 21:20:56 +0300

    Task 1 bi done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+            Task1();
            }
+
+    private static void Task1() {
+        System.out.println("\n==========[ Task 1 ]==========");
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d\t",i);
+        }
+    }
 }
