78fac4e6584655e5e6bfd8875681349c2164d9d0
Maxim <mqithelp@yandex.ru>
2024-06-12 15:35:13 +0300

    Task 3 is done;

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,27 @@ public class Main {
     public static void main(String[] args) {
         Task1();
         Task2();
+        Task3();
+    }
+
+    private static void Task3() {
+        System.out.println("\n\n==========[ Task 3 ]==========");
+        int totalY = 12_000_000;
+        for (int i = 1; i <= 10; i++) {
+            totalY += totalY / 1000 * 17 - totalY / 1000 * 8;
+            System.out.printf("Год %d, численность населения составляет %d.\n", i, totalY);
+        }
     }
 
     private static void Task2() {
         System.out.println("\n\n==========[ Task 2 ]==========");
         int i = 1;
-        while (i <= 10){
-            System.out.printf("%d\t",i++);
+        while (i <= 10) {
+            System.out.printf("%d\t", i++);
         }
         System.out.println();
-        for (int j = 10; j > 0 ; j--) {
-            System.out.printf("%d\t",j);
+        for (int j = 10; j > 0; j--) {
+            System.out.printf("%d\t", j);
         }
     }
 
