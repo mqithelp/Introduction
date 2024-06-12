e167650fa6f877e241b0df15b3373adfcfc9de39
Maxim <mqithelp@yandex.ru>
2024-06-12 15:11:40 +0300

    Task 1 is done;

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        Task1();
+    }
+
+    private static void Task1() {
+        System.out.println("\n\n==========[ Task 1 ]==========");
+        int totalCash = 2_459_000;
+        int incCash = 15_000;
+        int month = 0;
+        int monthCash = 0;
+        while (totalCash > monthCash) {
+            month++;
+            monthCash += incCash;
+            System.out.printf("Месяц %d, сумма накоплений равна %d рублей.\n", month, monthCash);
+        }
 
     }
 
