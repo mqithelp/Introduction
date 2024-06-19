f85dfe9b96805e0fa7aeb399dff46a0b3d0c7089
Maxim <mqithelp@yandex.ru>
2024-06-19 20:18:39 +0300

    Task 2,3 is done.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,29 @@ public class Main {
     public static void main(String[] args) {
         int[] records = {12000, 34000, 27000, 30000, 17000};
         task1(records);
+        task2(records);
+        task3(records);
+    }
+
+    private static void task3(int[] records) {
+        System.out.println("\n==========[ Task 3 ]==========");
+        final int  WEAK_OF_MOUNTH = records.length;
+        int sum = 0;
+        for (int i : records) sum += i;
+        float avg_money = (float) sum / WEAK_OF_MOUNTH;
+        System.out.printf("Средняя сумма трат за месяц составила %f руб.\n", avg_money);
+    }
+
+    private static void task2(int[] records) {
+        System.out.println("\n==========[ Task 2 ]==========");
+        int min = records[0];
+        int max = records[0];
+        for (int i : records) {
+            if (i < min) min = i;
+            if (i > max) max = i;
+        }
+        System.out.printf("«Минимальная сумма трат за неделю составила %d рублей. \n" +
+                "Максимальная сумма трат за неделю составила %d рублей.\n", min, max);
     }
 
     private static void task1(int[] records) {
