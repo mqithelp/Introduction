7f76be0295de45d4e80eda69b01938cea9da82ce
Maxim <mqithelp@yandex.ru>
2024-06-19 19:56:08 +0300

    Task 1 is done.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        int[] records = {12000, 34000, 27000, 30000, 17000};
+        task1(records);
+    }
 
+    private static void task1(int[] records) {
+        System.out.println("\n==========[ Task 1 ]==========");
+        int sum = 0;
+        for (int i : records) {
+            sum += i;
+        }
+        System.out.printf("Сумма трат за месяц составила %d рублей.\n", sum);
     }
 
 
