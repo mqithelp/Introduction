fc7ee56aef165a505571340711309512b082e1fa
mqWork <mqithelp@gmail.com>
2024-06-24 12:37:41 +0300

    All done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+
+    }
+
+    private static void task3() {
+        System.out.println("\n\n==========[ Task 3 ]==========");
+        String fullname = "Иванов Семён Семёнович";
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullname.replace('ё','e'));
+    }
+
+    private static void task2() {
+        System.out.println("\n\n==========[ Task 2 ]==========");
+        String fullName ="Ivanov Ivan Ivanovich";
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+    }
+
+    private static void task1() {
+        System.out.println("\n\n==========[ Task 1 ]==========");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
 
     }
 
