a52bb1fc3fbece54a762d1e38129437da2d5ce29
MaxHome <mqithelp@gmail.com>
2024-06-26 23:19:38 +0300

    Task 2 is done.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         calculateLeapYear(1976); //Task 1
+        downloadApp(2015,0); //Task 2
+        sendCard(95);
+    }
 
+    private static void sendCard(int deliveryDistance) {
 
+    }
+
+    private static void downloadApp(int clientDeviceYear, int clientOS) {
+        printNumberTask(2);
+        if (clientDeviceYear < 2015) {
+            if (clientOS == 0) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        } else {
+            if (clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
     }
 
     private static void calculateLeapYear(int year) {
@@ -17,8 +38,8 @@ public class Main {
 
         }
 
-    private static void printNumberTask(int n) {
-        System.out.printf("\n==========[ Task %d ]==========\n",n);
+    private static void printNumberTask(int numberTask) {
+        System.out.printf("\n==========[ Task %d ]==========\n",numberTask);
     }
 }
 
