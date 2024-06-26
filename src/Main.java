ae1a33ba51d7a6cf542c76b2a094418ebb7acc36
Maxim <mqithelp@yandex.ru>
2024-06-26 22:49:27 +0300

    Interval commit.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        task1(1976);
 
     }
 
+    private static void task1(int year) {
+        printNumberTask(1);
+            if (year > 1584 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
+                System.out.printf("%d год является високосным.\n", year);
+            } else if (year > 1584) {
+                System.out.printf("%d год не является високосным.\n", year);
+            } else {
+                System.out.println("Введен некорректный год.");
+            }
+
+        }
+
+    private static void printNumberTask(int n) {
+        System.out.printf("\n==========[ Task %d ]==========\n",n);
+    }
 }
+
