f46f732b5488a25ec4e716c0bd83c39569de7502
Maxim <mqithelp@yandex.ru>
2024-07-01 13:13:01 +0300

    Init commit with class.

---
+++ src/Book.java
@@ -0,0 +1,5 @@
+public class Book {
+    String title;
+    Author author;
+    int year;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        Book bookOne = new Book();
+        Book bookTwo = new Book();
     }
 
 
