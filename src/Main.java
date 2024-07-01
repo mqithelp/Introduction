a67aeba3413c7109483070edd9eb0363fd0b5840
MaxHome <mqithelp@gmail.com>
2024-07-01 18:22:00 +0300

    Task is done.

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ public class Main {
         Author authorAlexandrBeljaev = new Author("Александр", "Беляев");
         Book bookOne = new Book("Таинственный остров", authorJulesVerne, 1975);
         Book bookTwo = new Book("Продавец воздуха", authorAlexandrBeljaev, 1929);
-        System.out.println("bookOne = " + bookOne);
-        System.out.println("bookTwo = " + bookTwo);
+
     }
 
 }
