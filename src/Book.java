7779037c48a0c91408a08351202e5178e550ab5e
MaxHome <mqithelp@gmail.com>
2024-07-03 21:26:44 +0300

    Task is done.

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
 
     private String title;
@@ -31,5 +33,18 @@ public class Book {
         return "Книга \'" + title + '\'' +
                 " написана автором: " + author +
                 " в " + publicationYear +" году.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publicationYear == book.publicationYear && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, publicationYear);
     }
 }
